77f635d37998af646eb20efa74411d073ceef23c
Chris <87422346+Delloarc@users.noreply.github.com>
2023-04-02 23:46:40 +0200

    Add files via upload

--- website/views.py
+++ website/views.py
@@ -1,14 +1,8 @@
-from flask import Blueprint, render_template, Flask
-from flask_frozen import Freezer
+from flask import Blueprint, render_template
 
-app = Flask(__name__)
-freezer = Freezer(app)
 # Defines Blueprint
 views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
   return render_template("home.html")
-
-if __name__ == '__main__':
-    freezer.freeze()
