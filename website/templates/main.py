7fabdf1691ed17b3bfd02509a9f84f130d6f2ecb
Chris <87422346+Delloarc@users.noreply.github.com>
2023-04-02 21:52:36 +0200

    Delete website directory

--- website/templates/main.py
+++
@@ -1,7 +0,0 @@
-from website import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-  # When making changes to python code this automatically reruns the web server
-  app.run(debug=True) 
